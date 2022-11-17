9b6a3afa6651409410cc9f9b58a2335c38a416e5
Adriel Nazar Santos <99226370+adrielnazar@users.noreply.github.com>
2022-11-17 02:10:31 -0300

    Adição de argumentos.

--- trabalho1.py
+++ trabalho1.py
@@ -1,4 +1,6 @@
-#Inportação das bibliotecas
+#Usei esse código no Google Colab, mas antes passei o codigo "pip install tensorflow==2.7.0." para não ter nenhum erro.
+
+# Inportação das bibliotecas
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 from tensorflow.keras.models import Sequential
@@ -9,33 +11,39 @@ from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# Preparando um conjunto para reconhecer o padrão de escrita de números.
 print('[INFO] accessing MNIST...')
 ((trainX, trainY), (testX, testY)) = mnist.load_data()
 
+# Normalização os dados entre 0 e 1, dividindo o conjunto por 255.
 trainX = trainX.reshape((trainX.shape[0], 28 * 28 * 1))
 testX = testX.reshape((testX.shape[0], 28 * 28 * 1))
 trainX = trainX.astype('float32') / 255.0
 testX = testX.astype('float32') / 255.0
 
+# Binarização da classe para adequar a última camada.
 lb = LabelBinarizer()
 trainY = lb.fit_transform(trainY)
 testY = lb.transform(testY)
 
+# Definição da arquitetura da rede neural.
 model = Sequential()
 model.add(Dense(256, input_shape=(784,), activation='sigmoid'))
 model.add(Dense(128, activation='sigmoid'))
 model.add(Dense(10, activation='softmax'))
 
+# Preparar a rede para convergir e apreender, e vamos apresentando uma quantia de imagens por iteração.
 sgd = SGD(0.01)
 model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=['accuracy'])
 H = model.fit(trainX, trainY, validation_data=(testX, testY),epochs=100, batch_size=128)
 
+# Verificação de como a rede neural se saiu comparando os valores preditos com os reais.
 predictions = model.predict(testX, batch_size=128)
 print(classification_report(testY.argmax(axis=1),
     predictions.argmax(axis=1),
     target_names=[str(x) for x in lb.classes_]))
 
+# Verificação de como a rede neural se saiu.
 plt.style.use('ggplot')
 plt.figure()
 plt.plot(np.arange(0, 100), H.history['loss'], label='train_loss')
